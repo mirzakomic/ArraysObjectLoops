b0641c32355adf9159d53540a274c96235ce41fe
mirzakomic <komic.mirza@gmail.com>
2023-05-17 15:39:22 +0200

    zwei zusätzliche uebungen

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,45 @@ let studentData = [
 studentData.forEach((student) => {
 console.log(student.name, student.coop, student.address.city, student.emails);
 });
+
+//! Bonus
+
+const singers = [
+	{ name: 'The Beatles', country: 'United Kingdom', period_active: { start: 1960, end: 1970 }, genre: "Rock / Pop" },
+	{ name: 'Elvis Presley', country: 'United States', period_active: { start: 1954, end: 1977 }, genre: "Rock and roll" },
+	{ name: 'Michael Jackson', country: 'United States', period_active: { start: 1964, end: 2009 }, genre: "Pop / Rock / Dance / Soul / R&B"},
+	{ name: 'Elton John', country: 'United Kingdom', period_active: { start: 1964, end: "present" }, genre: "Pop / Rock"},
+	{ name: 'Madonna', country: 'United States', period_active: { start: 1979, end: "present" }, genre: "Pop / Dance / Electronica"},
+	{ name: 'Led Zeppelin', country: 'United Kingdom', period_active: { start: 1968, end: 1980 }, genre: "Hard rock / Blues rock / Folk rock"},
+	{ name: 'Rihanna', country: 'United States', period_active: { start: 2005, end: "present" }, genre: "R&B / Pop / Dance / Hip-hop"},
+	{ name: 'Pink Floyd', country: 'United Kingdom', period_active: { start: 1965, end: 1996, extra: 2014 }, genre: "Progressive rock / Psychedelic rock"},
+  ];
+
+singers.sort((a,b) => a.name > b.name ? 1 : -1);
+console.log(singers);
+
+//! Bonus
+
+let nameTab = document.querySelector("#name");
+let preisTab = document.querySelector("#preis");
+let diffTab = document.querySelector("#differenz");
+
+let edelMetallPreise = [
+  { name: "Gold", preiseGramEuro: 42.91, veraenderung: "+0.12%" },
+  { name: "Silber", preiseGramEuro: 0.51, veraenderung: "+1.02%" },
+  { name: "Platin", preiseGramEuro: 25.74, veraenderung: "+0.41%" },
+  { name: "Palladium", preiseGramEuro: 50.93, veraenderung: "0.00%" },
+  { name: "Rhodium", preiseGramEuro: 160.12, veraenderung: "-0.10%" },
+  { name: "Iridium", preiseGramEuro: 42.84, veraenderung: "0.00%" },
+  { name: "Ruthenium", preiseGramEuro: 7.36, veraenderung: "0.00%" },
+  { name: "Rhenium", preiseGramEuro: 37.22, veraenderung: "-0.53%" },
+  { name: "Osmium", preiseGramEuro: 11.54, veraenderung: "0.00%" }
+];
+
+
+edelMetallPreise.map((name) => {
+  nameTab.innerHTML += `<tr><td>${name.name}</td></tr> </br>`;
+  preisTab.innerHTML += `<tr><td>${name.preiseGramEuro}</td></tr>  </br>`;
+  diffTab.innerHTML += `<tr><td>${name.veraenderung}</td></tr>  </br>`;
+});
+
